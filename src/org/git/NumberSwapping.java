7c610dd01dc7aa25a16dbcfb64feb1e7fb69305d
praveenlakshmmanan287 <praveenlakshmanan287@gmail.com>
2022-09-22 16:19:02 +0530

    new code

---
+++ src/org/git/NumberSwapping.java
@@ -0,0 +1,25 @@
+package org.git;
+
+public class NumberSwapping {
+	
+
+		public static void main(String[] args) {
+
+			int a=10;
+			int b=20;
+			System.out.println("before swapping");
+			System.out.println("a is"+a);
+			System.out.println("b is"+b);
+
+			
+			//swapping number using 3rd variable
+			
+			int c=a;
+			a=b;
+			b=c;
+			System.out.println("after swapping");
+			System.out.println("a is"+a);
+		    System.out.println("b is"+b);
+
+}
+}
